4f22c20af39fafb30fd522ea9340f5f506b7d837
matronae <mykaomas@gmail.com>
2024-03-05 16:01:50 -0600

    added get route for user

---
+++ routes/userRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const User = require('../models/user');
+
+const router = express.Router();
+
+// GET all users
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
